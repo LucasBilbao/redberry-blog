b2ff7d66f76d11183b2ff2f6452283e66a63bc54
LucasBilbao <lucasbm789@gmail.com>
2023-12-19 18:08:30 +0400

    feat: change initial project settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'rb-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'redberry-blog';
