107b2b75e78ceca5b8065a29e8e880f74265d886
LucasBilbao <lucasbm789@gmail.com>
2023-12-24 16:07:00 +0400

    feat: add authorized logic for header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ModalService } from '../../../services/modal/modal.service';
+import { LoginService } from '../../../services/login/login.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'rb-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  public isAuthorized$: Observable<boolean> = this.loginService.isAuthorized$;
 
+  constructor(
+    private modalService: ModalService,
+    private loginService: LoginService
+  ) {}
+
+  public openModal(): void {
+    this.modalService.openModal();
+  }
 }
