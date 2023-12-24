3b5bd5c646023e8ea50594ea9f8c3aa7a73d8505
LucasBilbao <lucasbm789@gmail.com>
2023-12-24 19:50:32 +0400

    feat: implement filtering usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 import { BlogsStateFacade } from './store/facades/blogs.facade';
 import { CategoriesStateFacade } from './store/facades/categories.facades';
 import { ModalService } from './services/modal/modal.service';
+import { ActivatedRoute } from '@angular/router';
+import { FilterService } from './services/filter/filter.service';
 
 @Component({
   selector: 'rb-root',
@@ -19,11 +21,25 @@ export class AppComponent implements OnInit {
     private loadingService: LoadingService,
     private blogsFacade: BlogsStateFacade,
     private categoriesFacade: CategoriesStateFacade,
-    private modalService: ModalService
+    private modalService: ModalService,
+    private route: ActivatedRoute,
+    private filterService: FilterService
   ) {}
 
   public ngOnInit(): void {
     this.blogsFacade.getAllBlogs();
     this.categoriesFacade.getAllCategories();
+
+    this.route.queryParams.subscribe((queries) => {
+      if (!queries['categories']) {
+        return;
+      }
+
+      const categories = queries['categories']
+        .split(',')
+        .map((category: string) => Number.parseInt(category));
+
+      this.filterService.selectedCategories$ = new Set(categories);
+    });
   }
 }
